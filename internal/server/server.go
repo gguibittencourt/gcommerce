1ad375038ba6d0db4114cfb6dd3af322ebb9309d
Guilherme Bittencourt da Silva <gguibittencourt@gmail.com>
2023-09-11 22:05:36 -0300

    Change to use go-chi instead gin-gonic

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,21 +2,38 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5"
 	"go.uber.org/fx"
 )
 
-func StartHTTPServer(l fx.Lifecycle) {
-	l.Append(fx.Hook{
-		OnStart: func(c context.Context) error {
+func NewHTTPRouter() *chi.Mux {
+	return chi.NewRouter()
+}
+
+func StartHTTPServer(lifecycle fx.Lifecycle, router *chi.Mux) {
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  5000 * time.Second,
+		WriteTimeout: 5000 * time.Second,
+	}
+	lifecycle.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			fmt.Println("Starting server...")
 			go func() {
-				router := gin.Default()
-				if err := router.Run(); err != nil {
-					panic(err)
+				err := server.ListenAndServe()
+				if err != nil {
+					fmt.Printf("Error starting server: %s\n", err.Error())
 				}
 			}()
 			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return server.Shutdown(ctx)
 		},
 	})
 }
